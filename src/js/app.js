e40c3e0ad5235b8c6639fb9fb7c8baa4a314cce1
Morteza <mortazapor3@gmail.com>
2023-07-31 01:58:53 +0330

    refactor(app): Format javascript codes with prettier

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
-const handleNavbarPosition = (function (){
-	const navbarElem = document.querySelector('nav');
+const handleNavbarPosition = (function () {
+  const navbarElem = document.querySelector('nav');
 
-	return function(){
-		const scrollTop = document.documentElement.scrollTop;
+  return function () {
+    const scrollTop = document.documentElement.scrollTop;
 
-		navbarElem.classList[scrollTop <= 0 ? 'remove' : 'add']('active');
-	}
-})()
+    navbarElem.classList[scrollTop <= 0 ? 'remove' : 'add']('active');
+  };
+})();
 
 window.addEventListener('load', handleNavbarPosition);
 window.addEventListener('scroll', handleNavbarPosition);
