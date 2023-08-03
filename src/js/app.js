70d45bef670947e77ff5e9ac4f66fcf04c9c6153
Morteza <mortazapor3@gmail.com>
2023-08-03 11:06:29 +0330

    feat(app): Complete website mobile viewport styling

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 const handleNavbarPosition = (function () {
   const navbarElem = document.querySelector('nav');
 
@@ -10,3 +18,20 @@ const handleNavbarPosition = (function () {
 
 window.addEventListener('load', handleNavbarPosition);
 window.addEventListener('scroll', handleNavbarPosition);
+
+// Swiper Initialize
+(function () {
+  const swiper = new Swiper('.swiper', {
+    pagination: {
+      el: '.pagination',
+      bulletClass: 'pagination__bullet',
+      bulletActiveClass: 'pagination__bullet--active',
+      bulletElement: 'div',
+    },
+    modules: [Navigation, Pagination],
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+})();
